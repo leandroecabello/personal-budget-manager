53fdb10f61aad8ee67ed7d0e45aefc4f3f2df75e
leandro <leandro.ecabello@gmail.com>
2020-10-27 17:00:48 -0300

    changed jwt-expiresTime

--- Backend/src/app/libs/token.js
+++ Backend/src/app/libs/token.js
@@ -6,7 +6,7 @@ const generateToken = (user) => {
   const payload = {
     userId: user.id,
     createdAt: moment().unix(),
-    expiredAt: moment().add(5, "minutes").unix(),
+    expiredAt: moment().add(JWT.EXPIRES_TIME, "minutes").unix(),
   };
 
   return sign(JSON.stringify(payload), JWT.PRIVATE_KEY);
