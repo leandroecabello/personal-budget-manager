0fd1e04d42fc8a0f1b056a617e20d57804fce677
leandro <leandro.ecabello@gmail.com>
2020-10-27 19:13:33 -0300

    defining structure

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { ListOperationsComponent } from './components/list-operations/list-operations.component';
+import { LoginComponent } from './components/login/login.component';
+import { OperationComponent } from './components/operation/operation.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'addOperation', component: OperationComponent},
+  {path: 'operations', component: ListOperationsComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Frontend/src/app/services/Global.ts
@@ -0,0 +1,3 @@
+export const GLOBAL = {
+    URL: 'https://localhost:3000/api'
+};
